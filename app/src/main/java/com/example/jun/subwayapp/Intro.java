44cc3b2e0e9915c99af8df59aba2f15ec14b3331
heak <chjgurwns@naver.com>
2017-03-23 20:05:54 +0900

    인트로 화면 추가

---
+++ app/src/main/java/com/example/jun/subwayapp/Intro.java
@@ -0,0 +1,25 @@
+package com.example.jun.subwayapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Intro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Intro.this, TestActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
