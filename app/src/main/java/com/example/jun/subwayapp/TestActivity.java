13dca1f6205fe10b7b7a232b4aa6ddafc92d8306
Mirae Park <pmr930105@gmail.com>
2017-03-27 17:44:19 +0900

    adjust icon with navi menu
    잘안된다ㅠㅠ

--- app/src/main/java/com/example/jun/subwayapp/TestActivity.java
+++ app/src/main/java/com/example/jun/subwayapp/TestActivity.java
@@ -55,17 +55,11 @@ public class TestActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.t1) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.t2) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.t3) {
 
         }
 
